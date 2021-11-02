984b3c1302f8a6648e19ef6a868d2b84a1336c46
Bakr <b.annour@stuart.com>
2021-11-02 15:30:07 +0000

    -

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import json
 import os
 app = Flask(__name__)
@@ -11,15 +11,46 @@ def home():
     return 'R&I 6'
 
 @app.route('/posts', methods=['GET'], defaults={'postid': None})
-@app.route('/posts/<postid>', methods=['GET'])
+@app.route('/posts/<int:postid>', methods=['GET'])
 def query_posts(postid):
     filename = os.path.join(app.static_folder, 'posts.json')
     with open(filename) as f:
         records = json.load(f)
     if postid:
-        "Insert your code here"
+        for item in records:
+            if item['id'] == postid:
+                return jsonify(item)
+        abort(404)
     else:
-        "Insert your code here"
+        return jsonify(records)
+
+@app.route('/comments', methods=['GET'], defaults={'comment_id': None})
+@app.route('/comments/<int:comment_id>', methods=['GET'])
+def query_comments(comment_id):
+    filename = os.path.join(app.static_folder, 'comments.json')
+    with open(filename) as f:
+        records = json.load(f)
+    if comment_id:
+        for item in records:
+            if item['id'] == comment_id:
+                return jsonify(item)
+        abort(404)
+    else:
+        return jsonify(records)
+
+@app.route('/users', methods=['GET'], defaults={'user_id': None})
+@app.route('/users/<int:user_id>', methods=['GET'])
+def query_users(user_id):
+    filename = os.path.join(app.static_folder, 'users.json')
+    with open(filename) as f:
+        records = json.load(f)
+    if user_id:
+        for item in records:
+            if item['id'] == user_id:
+                return jsonify(item)
+        abort(404)
+    else:
+        return jsonify(records)
 
 ## ADD COMMENTS AND USERS METHODS HERE
 
